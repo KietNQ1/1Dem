160289b016723d66745f02a68cae54a6e82b372e
KietNQ1 <qkiet2kk4@gmail.com>
2025-06-10 23:36:03 +0700

    khoi tao

---
+++ 1DemCung2A/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1DemCung2A.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
